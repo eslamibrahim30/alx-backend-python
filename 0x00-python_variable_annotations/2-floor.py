2d246222a0e6ad57708c4514cfd01f5b20dc03e9
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-14 21:13:38 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module for task "Basic annotations - floor"
+"""
+
+
+def floor(n: float) -> int:
+    """ This function returns the floor of a number """
+    return int(n)

708211fed8835694b1a128d96f0c25f5ffb338ff
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-03-14 21:09:26 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module for task "Basic annotations - concat"
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function concatenates two strings """
+    return str1 + str2
